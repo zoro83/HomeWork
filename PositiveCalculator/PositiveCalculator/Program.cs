2aebbf6ac68f6507d7b4d6fe134aba231f6e7c9a
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-09-30 03:12:39 +0400

    update

--- PositiveCalculator/PositiveCalculator/Program.cs
+++ PositiveCalculator/PositiveCalculator/Program.cs
@@ -21,48 +21,75 @@ namespace PositiveCalculator
             Console.Write("Num2 = ");
             inputNum2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Select any operation ABS \n'+', '-', '*', '/', '%', 'Power', 'Square root'");
+            Console.WriteLine($"Select any operation (ABS) \n'+', '-', '*', '/', '%', 'Power', 'Square root'");
             operation = Console.ReadLine();
-            //Console.WriteLine($"Num1 {operation} Num2 = {inputNum1} {operation} {inputNum2}");
-            if (operation == "+") {
-                Console.WriteLine($"ABS(Num1 {operation} Num2) = {Math.Abs(inputNum1 + inputNum2)}");
-            }
-            if (operation == "-")
-            {
-                Console.WriteLine($"ABS(Num1 {operation} Num2) = {Math.Abs(inputNum1 - inputNum2)}");
-            }
-            if (operation == "*")
-            {
-                Console.WriteLine($"ABS(Num1 {operation} Num2) = {Math.Abs(inputNum1 * inputNum2)}");
-            }
-            if (operation == "/")
-            {
-                if (inputNum2 != 0)
+
+            if (operation == "+" || operation == "-" || operation == "*" || operation == "/"
+                || operation == "%" || operation == "Power" || operation == "power"
+                || operation == "Square root" || operation == "square root" || operation == "square Root"
+                || operation == "Square Root") // Main if  
+            {       
+
+                if (operation == "+")         // ( + )operation
                 {
-                    Console.WriteLine($"ABS(Num1 {operation} Num2) = {Math.Abs((inputNum1*1.0d) / inputNum2)}");
+                    Console.WriteLine($"ABS(Num1 {operation} Num2) = {Math.Abs(inputNum1 + inputNum2)}");
                 }
-                else {
-                    Console.WriteLine("Divided by 0 exception");
+
+                if (operation == "-")          // ( - )operation
+                {
+                    Console.WriteLine($"ABS(Num1 {operation} Num2) = {Math.Abs(inputNum1 - inputNum2)}");
                 }
-                //Console.WriteLine($"ABS(Num1 {operation} Num2) = {Math.Abs(inputNum1 * inputNum2)}");
-            }
-            // Math Power
-            if (operation == "Power" || operation == "power") {       // case insensitive
-                Console.Write("Input Power of Num1: ");
-                double powerNum1 = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Input Power of Num2: ");
-                double powerNum2 = Convert.ToDouble(Console.ReadLine());
+                if (operation == "*")      // ( * )operation
+                {
+                    Console.WriteLine($"ABS(Num1 {operation} Num2) = {Math.Abs(inputNum1 * inputNum2)}");
+                }
 
-                Console.WriteLine($"ABS(Num1)^{powerNum1}) = {Math.Abs(Math.Pow(inputNum1, powerNum1))}");
-                Console.WriteLine($"ABS(Num2)^{powerNum2}) = {Math.Abs(Math.Pow(inputNum2, powerNum2))}");
-            }
-            // Square root
-            if (operation == "Square root" || operation == "square root" || operation == "square Root" || operation == "Square Root")
-            // case insensitive
-            {
-                Console.WriteLine($"Root of ABS(Num1) = {Math.Sqrt(Math.Abs(inputNum1 * 1.0d))}");
-                Console.WriteLine($"Root of ABS(Num2) = {Math.Sqrt(Math.Abs(inputNum2 * 1.0d))}");
+                if (operation == "/")   // ( / )operation
+                {
+                    if (inputNum2 != 0)
+                    {
+                        Console.WriteLine($"ABS(Num1 {operation} Num2) = {Math.Abs((inputNum1 * 1.0d) / inputNum2)}");
+                    }
+                    else {
+                        Console.WriteLine("Divided by 0 exception");
+                    }
+
+                }
+
+                if (operation == "%")   // ( % )Operation
+                {
+                    if (inputNum2 != 0)
+                    {
+                        Console.WriteLine($"ABS(Num1 {operation} Num2) = {Math.Abs((inputNum1 * 1.0d) % inputNum2)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Divided by 0 exception");
+                    }
+                }
+
+
+
+                // Math Power
+                if (operation == "Power" || operation == "power") {       // case insensitive
+                    Console.Write("Input Power of Num1: ");
+                    double powerNum1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Input Power of Num2: ");
+                    double powerNum2 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine($"ABS(Num1)^{powerNum1}) = {Math.Abs(Math.Pow(inputNum1, powerNum1))}");
+                    Console.WriteLine($"ABS(Num2)^{powerNum2}) = {Math.Abs(Math.Pow(inputNum2, powerNum2))}");
+                }
+                // Square root
+                if (operation == "Square root" || operation == "square root" || operation == "square Root" || operation == "Square Root")
+                // case insensitive
+                {
+                    Console.WriteLine($"Root of ABS(Num1) = {Math.Sqrt(Math.Abs(inputNum1 * 1.0d))}");
+                    Console.WriteLine($"Root of ABS(Num2) = {Math.Sqrt(Math.Abs(inputNum2 * 1.0d))}");
+                }
+
             }
 
             else {
