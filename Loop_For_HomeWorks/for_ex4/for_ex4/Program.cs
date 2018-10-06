929552a96c83312262605f0b8607329ddb0f2687
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-06 04:37:08 +0400

    output all even numbers <=n

---
+++ Loop_For_HomeWorks/for_ex4/for_ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Input n integer number \n = ");
+            n = Convert.ToInt32
+                (Console.ReadLine());
+            for (int i = 0; i <= n; i+=2)
+            {
+                Console.Write("'{0}'", i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
