6c2dc042d98d3c529a29164c01e9641d0e50d61f
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-06 07:11:23 +0400

    Fibonacci Number

---
+++ Loop_For_HomeWorks/Fibonacci_Numbers/Fibonacci_Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This program prints Fibonacci numbers \nInput count N = ");
+            int N;
+            N = int.Parse(Console.ReadLine());
+            int x = 0;
+            int y = 1;
+            for (int i = 0; i < N; i ++)
+            {
+                int fibon = x;
+                x = y;
+                y = y + fibon;
+                Console.WriteLine($"'{fibon}'");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
