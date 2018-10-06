381ba599f24118205141ba6bc359e8e7d91e7c55
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-06 06:11:59 +0400

    Ex. 6,7,8 [A,B] segment count, sum multiply3 numbers

--- Loop_For_HomeWorks/for_ex5/for_ex5/Program.cs
+++ Loop_For_HomeWorks/for_ex5/for_ex5/Program.cs
@@ -10,22 +10,39 @@ namespace for_ex5
     {
         static void Main(string[] args)
         {
+            // exercise 6,7,8 all together
             int A;
             int B;
             Console.Write("Input A and B (A < B) integer numbers \nA = ");
             A = Convert.ToInt32(Console.ReadLine());
             Console.Write("B = ");
             B = Convert.ToInt32(Console.ReadLine());
-            int Sum = 0;
+            int count = 0;
+            int sum = 0;
+            int multiplyBy3 = 0;
             if (A < B)
-            {
-                for (int i = A; i <= B; i++)
+            {   // start of exercise 6, 7
+                for (int i = A; i <= B; i++) 
                 {
-                    Sum ++;
+                    count ++;
+                    sum += i;
                     Console.WriteLine($"{i} belong to [A, B]");
 
                 }
-                Console.WriteLine($"Count integer numbers in [A, B] segment is equal to {Sum}");
+                Console.WriteLine($"Count of integer numbers in [A, B] segment is equal to {count}");
+                // end of exercise 6
+                Console.WriteLine($"Sum of integer numbers in [A, B] segment is equal to {sum}");
+                // end of exercise 7
+                // start of exercise 8
+                for (int i = A; i <= B; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine($"The first multiplying by 3 numbers of the [A, B] segment is equal to'{i}'");
+                        break;
+                    }                   
+                }
+                // end of exercise 8                
             }
             else {
                 Console.WriteLine("Incorrect input");
