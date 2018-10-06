5e927a9efc2c924d0d07e4592b042aefd2e379d9
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-06 04:17:55 +0400

    output k < =n numbers

---
+++ Loop_For_HomeWorks/for_ex2/for_ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input n 1 digit number \nn = ");
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n >= 0 && n < 10)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    Console.Write($"'{i}' ");
+                }
+            }
+            else {
+                Console.WriteLine("Incorrect input");
+            }
+            Console.ReadKey();
+        }
+    }
+}
