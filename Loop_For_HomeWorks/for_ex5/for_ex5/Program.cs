dfd94db59dd1ac54c8d129ce7758700f5ff7af9e
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-06 05:04:15 +0400

    [A,B] all int num. and count

---
+++ Loop_For_HomeWorks/for_ex5/for_ex5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            Console.Write("Input A and B (A < B) integer numbers \nA = ");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B = ");
+            B = Convert.ToInt32(Console.ReadLine());
+            int Sum = 0;
+            if (A < B)
+            {
+                for (int i = A; i <= B; i++)
+                {
+                    Sum ++;
+                    Console.WriteLine($"{i} belong to [A, B]");
+
+                }
+                Console.WriteLine($"Count integer numbers in [A, B] segment is equal to {Sum}");
+            }
+            else {
+                Console.WriteLine("Incorrect input");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
