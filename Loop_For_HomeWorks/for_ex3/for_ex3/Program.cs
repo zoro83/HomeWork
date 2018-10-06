8f2c466556b65928b592cd7a40bbfa5815417a77
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-06 04:30:45 +0400

    output n^(1-9)

---
+++ Loop_For_HomeWorks/for_ex3/for_ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input n integer number \nn = ");
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine($"n^{i} = {Math.Pow(n, i)}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
