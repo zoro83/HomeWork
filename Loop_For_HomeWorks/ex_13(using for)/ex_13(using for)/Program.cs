3bd1d091a1c5bdc97a31497b7ee5e76c807ba8d8
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-08 02:48:53 +0400

    N2 = 1 + 3 + 5 + … + (2*N – 1)

---
+++ Loop_For_HomeWorks/ex_13(using for)/ex_13(using for)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_13_using_for_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            double sum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                sum += i * 2 - 1;
+                Console.WriteLine(i!=N? $"{i}^2 = {sum}":" ");
+            }
+            Console.Write("Final result \n{0}^2 = {1}", N, sum);
+            Console.ReadKey();
+        }
+    }
+}
