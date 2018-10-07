7109748ba400b82199bd3560228583ded2171a1a
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-08 02:18:23 +0400

    1.1 – 1.2 + 1.3 – …

---
+++ Loop_For_HomeWorks/ex_12(using for)/ex_12(using for)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_12_using_for_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            double sum = 1;
+            for (int i = 1; i <= N; i++)
+            {
+                sum += (1 + 1.0 * i / 10) * Math.Pow(-1, i+1);
+                //Console.WriteLine(sum);
+                Console.Write($"'{(1 + 1.0 * i / 10) * Math.Pow(-1, i + 1)}', ");
+            }
+            Console.WriteLine("\nN = {0} \n1.1 – 1.2 + 1.3 – … (N times) = {1}", N, sum);
+            Console.ReadKey();
+        }
+    }
+}
