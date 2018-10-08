e03271aae7014d2a197a245ed671a5871e34630e
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-09 01:21:51 +0400

    output k < =n numbers sum

---
+++ Loop_While_HomeWorks/ex_2(While)/ex_2(While)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_2_While_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input n 1 digit number \nn = ");
+            int n = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            int sum = 0;
+            while (i <= n && n / 10 == 0)
+            {
+                sum += i;
+                i++;
+            }
+            Console.WriteLine((n>=0)? $"{sum}":"incorrect format");
+            Console.ReadKey();
+        }
+    }
+}
