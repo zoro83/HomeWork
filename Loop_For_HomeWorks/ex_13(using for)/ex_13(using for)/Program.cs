55a05e1f679938c3efd9e82ecd531463a5fc355e
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-09 01:04:09 +0400

    n times print k

--- Loop_For_HomeWorks/ex_13(using for)/ex_13(using for)/Program.cs
+++ Loop_For_HomeWorks/ex_13(using for)/ex_13(using for)/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace ex_13_using_for_
 {
     class Program
@@ -15,7 +14,7 @@ namespace ex_13_using_for_
             for (int i = 1; i <= N; i++)
             {
                 sum += i * 2 - 1;
-                Console.WriteLine(i!=N? $"{i}^2 = {sum}":" ");
+                Console.WriteLine(i != N ? $"{i}^2 = {sum}" : " ");
             }
             Console.Write("Final result \n{0}^2 = {1}", N, sum);
             Console.ReadKey();
---
+++ Loop_While_HomeWorks/ex_1(while)/ex_1(while)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_1_while_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k;
+            int n = 0;
+            Console.Write("Input integer k and n (n > 0) \nk = ");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.Write("n = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i < n)
+            {
+                Console.WriteLine(k);
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
