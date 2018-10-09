8ffe5916e9b5311253dcf1ec1d223138d7518d8c
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-10 02:09:33 +0400

    Full and empty Rhombus

--- PictureApp/PictureApp/Program.cs
+++ PictureApp/PictureApp/Program.cs
@@ -14,7 +14,7 @@ namespace PictureApp
             N = Convert.ToInt32(Console.ReadLine());
             string visible = " + ";
             string unvisible = "   ";
-
+            //Full Rhombus
             for (int i = 0; i < N; i = Math.Abs(i) + 1)
             {
                 for (int j = 1; j <= 2 * N - 1; j++)
@@ -31,6 +31,24 @@ namespace PictureApp
                     }
                     Console.WriteLine();
             }
+            // empty Rhombus
+            for (int i = 0; i < N; i = Math.Abs(i) + 1)
+            {
+                for (int j = 1; j <= 2 * N - 1; j++)
+                {
+                    Console.Write((j == N - i || j == N + i) ? visible : unvisible);
+                }
+                Console.WriteLine();
+            }
+            for (int p = 1; p <= N - 1; p++)
+            {
+                for (int k = 1; k <= 2 * N - 1; k++)
+                {
+                    Console.Write((k == p + 1 || k == 2 * N - p -1) ? visible : unvisible);
+                }
+                Console.WriteLine();
+            }
+
             Console.ReadKey();
 
         }
