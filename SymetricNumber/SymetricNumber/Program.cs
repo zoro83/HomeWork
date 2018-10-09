87b5194f99e5c317b472b41455b472e137bf07b7
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-10 02:43:57 +0400

    Symetric number

---
+++ SymetricNumber/SymetricNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymetricNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This program cheked symetric numbers \nEnter number ");
+           string input = Console.ReadLine();
+            int num = int.Parse(input);
+            int digit = input.Length;
+            int i = 1;
+            bool flag = false;
+            while ((int)num /(int)Math.Pow(10,digit - i) % 10 == (int)num / (int)Math.Pow(10, i -1) % 10 && i <= digit / 2)
+            {
+                flag = true;
+                i++;
+            }
+            Console.WriteLine(flag?$"{num} is symetric number": $"{num} is not symetric number");
+            Console.ReadKey();
+        }
+    }
+}
