b22fce3a8c1777e20c6f785d4b5aa1e650c61538
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-11 15:11:50 +0400

    abc = 7*bc

---
+++ Static Methods in C#/ex_15/ex_15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_15
+{
+    class Program
+    {
+        static int Remove1stDigit(int n)
+        {
+            int result = n % 100;
+            //Console.WriteLine(result);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+             for (int i = 100; i < 1000; i++)
+            {
+                if (i == 7 * Remove1stDigit(i))
+                {
+                    Console.WriteLine(i);
+                }
+                //Console.WriteLine(Remove1stDigit(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
