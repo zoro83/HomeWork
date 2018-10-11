78223bb1df18eec158ace19519dc97fc7d2639ee
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-11 13:10:12 +0400

    Output number = 2 times sum of digits

---
+++ Binnary_to_Integer_and_vice versa/Binnary_to_Integer_and_vice versa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binnary_to_Integer_and_vice_versa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string intInput = Console.ReadLine();
+            int i = 0;
+            int decim = int.Parse(intInput);
+            i = decim;
+            int bin = 0;
+            while (i / 2 != 0)
+            {
+                bin = i % 2;
+                i /= 2;
+                Console.Write(bin);
+            }
+            Console.Write("1");
+        
+            
+            Console.ReadKey();
+        }
+       
+    }
+}
---
+++ Static Methods in C#/ex_14/ex_14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_14
+{
+    class Program
+    {
+        static int SumOfDigits(int n)
+        {
+            int N = Convert.ToString(n).Length;
+            int sum = 0;
+            int i = 1;
+            while (i <= N)
+            {
+                sum = sum + n / (int)Math.Pow(10, i - 1) % 10;
+                i++;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+
+            for (int i = 10; i < 100000; i++)
+            {
+                if (i == 2 * SumOfDigits(i))
+                {
+                    Console.WriteLine($"{i} is equal 2 times sum of digits");
+                }
+              
+            }
+            Console.ReadKey();
+        }
+    }
+}
