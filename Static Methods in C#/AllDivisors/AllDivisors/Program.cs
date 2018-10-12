08e16bf64a354837d0900d538747f2299707e73d
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-12 20:37:57 +0400

    Խնդիր_21-23

--- Static Methods in C#/AllDivisors/AllDivisors/Program.cs
+++ Static Methods in C#/AllDivisors/AllDivisors/Program.cs
@@ -15,17 +15,36 @@ namespace AllDivisors
         static void AllnNumberDivisors(int num)
         {
             int fixNum = num;
-            for (int i = 1; i <= Math.Pow(num,0.5); i++)
+            int sum = 0;
+            int j;
+            int count = 0;
+            double div = 0.0;
+            for (int i = 1; i <= (int)Math.Pow(num,0.5); i++)
             {
                 
                 if (num % i == 0)
                 {
-                    int j = num / i;
-                    Console.WriteLine((i != j)?$"'{i}' '{j}'":$"'{i}'");
+                    j = num / i;
+                    if (i != j)
+                    {
+                        Console.WriteLine($"'{i}' '{j}'");
+                        sum += i + j;
+                        count += 2;
+                    }
+                    else {
+                        Console.WriteLine($"'{i}'");
+                        sum += i;
+                        count += 1;
+
+                    }
+                   
                 }
                 
             }
-
+            Console.WriteLine("Sum of all the divisors of the given number {0} is equal {1} ",fixNum, sum);
+            Console.WriteLine("Count of all the divisors of the given number {0} is equal {1} ", fixNum, count);
+            div = 1.0 * sum / count;
+            Console.WriteLine($"Sum / Count is equal {div}");
 
         }
         static void Main(string[] args)
