3fa5e9766fd3e34e754d2d1f789d77905aa6d29b
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-15 23:54:25 +0400

    Factorial N!

---
+++ Recursion/Factorial/Factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        /// <summary>
+        /// Return N! factorial
+        /// </summary>
+        /// <param name="N">1,2,3,...</param>
+        /// <returns>N!</returns>
+        static long Factorial(int N)
+        {
+            if (N == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return N * Factorial(N - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N for counting N! \nN = ");
+            int N = byte.Parse(Console.ReadLine());
+            Console.WriteLine("{0}! = {1}", N, Factorial(N));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recursion/Factorial_!/Factorial_!/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;??
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial__1
+{
+    class Program
+    {
+        static long Factorial(int n)
+        {
+            return Factorial(n - 1) * n;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(Factorial(3));
+
+                
+        }
+    }
+}
