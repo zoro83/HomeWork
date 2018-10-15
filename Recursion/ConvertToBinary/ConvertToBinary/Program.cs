87693676ce35075f99ad69dee3617fdceceb4408
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-16 00:17:07 +0400

    Decimal number convert to binary

---
+++ Recursion/ConvertToBinary/ConvertToBinary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToBinary
+{
+    class Program
+    {
+        /// <summary>
+        /// Decimal number convert to binary
+        /// </summary>
+        /// <param name="num">int</param>
+        static void IntConvertToBinary(int num)
+        {
+            if (num / 2 != 0)
+            {
+                IntConvertToBinary(num / 2);
+                Console.Write(num % 2);
+            }
+            else Console.Write(1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N decimal number for convert to binary \nN = ");
+            int N = int.Parse(Console.ReadLine());
+            IntConvertToBinary(N);
+            Console.ReadKey();
+        }
+    }
+}
