dc14a7fb0cbf6636b15cafe47f982d453fe0ae97
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-18 00:25:24 +0400

    Խնդիր_29: abc = 7*bc

---
+++ ex_29/ex_29/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_29
+{
+    class Program
+    {
+        //Խնդիր_29:
+        //Եռանիշ թվի առաջին թվանշանը ջնջել։ Եթե ստացված թիվը բազմապատկվի 7-ով, կստացվի սկզբնական թիվը։ 
+        //Գտնել այդպիսի թվերը,  
+        //քանի որ այս խնդրի լուծումը առանց մասիվների ավելի կարճ է, նոր կոդ չեմ գրում:
+        static int Remove1stDigit(int n)
+        {
+
+            int result = n % 100;
+            //Console.WriteLine(result);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 100; i < 1000; i++)
+            {
+                if (i == 7 * Remove1stDigit(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
