47f644666d08d55933d109657b62c0740501523d
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-19 00:03:50 +0400

    Enter N for reversing and adding 0 between digits N

---
+++ ex_27/ex_27/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_27
+{
+    class Program
+    {
+        static string ReversAndNullBetweenDigits(int num)
+        {
+            byte N = (byte)Convert.ToString(num).Length;
+            string str = " ";
+            byte[] digit = new byte[2 * N];
+            for (byte i = 0; i < N; i ++)
+            {
+                digit[2 * i] = (byte)(num % 10);
+                Console.Write(digit[2 * i]);
+                digit[2 * i + 1] = 0;
+                Console.Write(digit[2 * i + 1]);
+                num /= 10;
+            }
+
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter N for reversing and adding 0 between digits N \nN = ");
+            int N = Int32.Parse(Console.ReadLine());
+            ReversAndNullBetweenDigits(N);
+
+            Console.ReadKey();
+        }
+    }
+}
