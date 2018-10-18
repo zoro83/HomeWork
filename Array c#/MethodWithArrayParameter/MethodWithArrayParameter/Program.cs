cdbadc5926d11e8df54124d3709d72c0b8e14f26
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-19 02:25:36 +0400

    Increasing or decreasing Array

---
+++ Array c#/MethodWithArrayParameter/MethodWithArrayParameter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithArrayParameter
+{
+    class Program
+    {
+        static void IncressingOrDecressingArray(int[] arr, bool flag)
+        {
+            int[] newArr = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (flag)
+                {
+                    int maxIndex = arr.ToList().IndexOf(arr.Max());
+                    newArr[i] = arr.Max();
+                    arr[maxIndex] = int.MinValue;
+                    Console.WriteLine(newArr[i]);
+                }
+                else
+                {
+                    int minIndex = arr.ToList().IndexOf(arr.Min());
+                    newArr[i] = arr.Min();
+                    arr[minIndex] = int.MaxValue;
+                    Console.WriteLine(newArr[i]);
+                }
+            }
+         
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 8, 3, 44, 77, -44, 1,-129};
+
+            Console.WriteLine("True");
+            IncressingOrDecressingArray(arr, true);
+
+            Console.WriteLine("False");
+            IncressingOrDecressingArray(arr, false);
+            Console.ReadKey();
+        }
+    }
+}
