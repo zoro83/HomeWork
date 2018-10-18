270dca621885e4daf3a47de4c505143a0faca164
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-18 23:23:55 +0400

    Խնդիր_30:

---
+++ Array c#/ex_30/ex_30/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_30
+{
+    class Program
+    {
+
+        /*static void FourDigitNumberNoRepeatedDigit()
+        {
+            for (short num = 1000; num < 9000;)
+            {
+                bool flag = true;
+                byte digitSum = 0;
+                byte[] digit = new byte[4];
+                byte[] TwoDigit = { (byte)(num % 100), (byte)(num / 100) };
+                for (byte i = 0; i < digit.Length; i++)
+                {
+                    digit[i] = (byte)(num % 10); /// abcd digit[0] = d, ...digit[3] = a
+                    num /= 10;
+                    digitSum += digit[i];
+                }
+                if (digit[0] != digit[1] && digit[0] != digit[2] && digit[0] != digit[3] && digit[1] != digit[2] && digit[1] != digit[3] && digit[2] != digit[3] && TwoDigit[1] - TwoDigit[0] == digitSum)
+                {
+                    Console.WriteLine(num);
+                    //Console.WriteLine(digit[0]);
+                    //Console.WriteLine(digit[1]);
+                    //Console.WriteLine(digit[2]);
+                    //Console.WriteLine(digit[3]);
+                    //Console.WriteLine(TwoDigit[0]);
+                    //Console.WriteLine(TwoDigit[1]);
+                }
+                else
+                    num++;
+            }
+        }*/
+
+        //// <summary>
+        /// Տպում է բոլոր այն քառանիշ թվերը, որոնցում կրկնվող թվանշան չկա և որոնցում առաջին և վերջին երկու թվանշաններից 
+        ///կազմված թվերի տարբերությունը հավասար է այդ թվի թվանշանների գումարին։
+        /// </summary>
+            static void OtherFunction()
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        for (int k = 0; k < 10; k++)
+                        {
+                            for (int q = 0; q < 10; q++)
+                            {
+                                int dif = 9 * i + 2 * j - 11 * k - 2 * q; 
+                                if (i != j && i != k && i != q && j != k && j != q && k != q && dif == 0)
+                                {
+                                    Console.WriteLine($"{i}{j}{k}{q}");
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        
+        static void Main(string[] args)
+        {
+            OtherFunction();
+            Console.ReadKey();
+                
+        }
+    }
+}
---
+++ Static Methods in C#/ex_30/ex_30/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_30
+{
+    class Program
+    {
+        static short FourDigitNumberNoDigitRepeat(short num)             
+            {
+            short[] digit = new short[4];
+            short[] twoDigit = { (short)(num / 100), (short)(num % 100) };
+            short digitSum = 0;
+            for (short i = 0; i < 4; i++)
+            {
+                digit[i] = (short)(num % 10);
+                num /= 10;
+                digitSum += digit[i];
+            }
+            
+            }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
