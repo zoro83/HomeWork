8d656d3b4680a9228a22b402431cfb424de314a6
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-22 00:50:36 +0400

    Return Sum of Sequence 1 + X + X2/(2!) + … + XN/(N!)

---
+++ Array c#/ex_33/ex_33/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_32
+{
+    class Program
+    {
+        //Տրված են X իրական և N բնական թվերը։ Գտնել 1 + X + X2/(2!) + … + XN/(N!) 
+        //արտահայտության արժեքը (N! = 1·2·…·N)։
+        /// <summary>
+        /// Return Sum of Sequence 1 + X + X2/(2!) + … + XN/(N!)
+        /// </summary>
+        /// <param name="X">double</param>
+        /// <param name="N">int</param>
+        /// <returns></returns>
+        static double SequenseSum(double X, int N)
+        {
+            double sum = 1;
+            double index = 1;
+            for (int i = 1; i <= N; i++)
+            {
+                index *= X / (double)(i);
+                sum += index;
+
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            //int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(SequenseSum(1.5, 22));
+            Console.ReadKey();
+
+        }
+    }
+}
