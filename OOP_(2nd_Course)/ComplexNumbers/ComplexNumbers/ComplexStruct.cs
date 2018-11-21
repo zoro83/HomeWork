01749f1e948b9d4ab29e88230d6f99081a96a005
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-22 00:17:35 +0400

    Complex Number Defination Using Struct

---
+++ OOP_(2nd_Course)/ComplexNumbers/ComplexNumbers/ComplexStruct.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers
+{
+    struct Complex
+    {
+        private double x;
+        private double y;
+        public Complex(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public string GetValue()
+        {
+            if (y > 0)
+            {
+
+                return (x != 0) ? $"{x} + {y}i" : $"{y}i";
+            }
+            else if (y < 0)
+            {
+                return (x != 0) ? $"{x} - {y * (-1)}i" : $"{y}i";
+            }
+            else
+            {
+                return $"{x}";
+            }
+        }
+        public void SetValue(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Complex Plus(Complex a, Complex b)
+        {
+            return new Complex(a.x + b.x, a.y + b.y);
+        }
+        public Complex Minus(Complex a, Complex b)
+        {
+            return new Complex(a.x - b.x, a.y - b.y);
+        }
+
+        public Complex Multiplication(Complex a, Complex b)
+        {
+            return new Complex(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x);
+        }
+        public Complex Division(Complex a, Complex b)
+        {
+            return new Complex((a.x * b.x + a.y * b.y) / (Math.Pow(b.x, 2) + Math.Pow(b.y, 2)),
+                (a.y * b.x - a.x * b.y) / (Math.Pow(b.x, 2) + Math.Pow(b.y, 2)));
+        }
+        public Complex Absalute(Complex a, Complex b)
+        {
+            return new Complex(Math.Pow((Math.Pow(b.x, 2) + Math.Pow(b.y, 2)), 0.5), 0);
+        }
+    }
+
+}
---
+++ OOP_(2nd_Course)/ComplexNumbers/ComplexNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers
+{
+    class Program
+    {
+        public static void Print(Complex num)
+        {
+            Console.WriteLine(num.GetValue());
+        }
+        static void Main(string[] args)
+        {
+            Complex X = new Complex(9, -2);
+            Print(X);
+            Complex Y = new Complex(0, 7);
+            Print(Y);
+            Complex Z = new Complex(-6, 0);
+            Print(Z);
+
+            X.SetValue(3, 3);
+            Print(X);
+            
+            Z = Z.Plus(X, Y);
+            Print(Z);
+
+            Z = Z.Minus(X, Y);
+            Print(Z);
+
+            Z = Z.Multiplication(X, Y);
+            Print(Z);
+
+            Z = Z.Absalute(X, Y);
+            Print(Z);
+
+            Console.ReadKey();
+        }
+    }
+}
