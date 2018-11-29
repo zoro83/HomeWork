96a26167de445ceb35cb5b6c358a1a4682a0a237
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-30 01:14:52 +0400

    Event Calculator Version 1

---
+++ OOP_(2nd_Course)/CalcEvent/CalcEvent/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcEvent
+{
+    public delegate float CalcDelegate(float num1, float num2);
+    class MyCalc
+    {
+        public event CalcDelegate CalcEvent = null;
+        public float EventInvoker(float num1, float num2)
+        {
+            return CalcEvent.Invoke(num1, num2);
+        }
+
+    }
+    class Program
+    {
+
+        public static float HandllerAdd(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+        public static float HandllerMinus(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+        public static float HandllerMultyple(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+        public static float HandllerDivide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+        static void Main(string[] args)
+        {
+            MyCalc myCalc = new MyCalc();
+            bool flag = false;
+            float sum = 0;
+            Console.WriteLine("***Input Number one and number two for operation***");
+            float num1 = float.Parse(Console.ReadLine());
+            char operation = char.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("=");
+            while (!flag)
+            {
+                switch (operation)
+                {
+                    case '+':
+                        myCalc.CalcEvent += HandllerAdd;
+                        sum = myCalc.EventInvoker(num1, num2);
+                        Console.WriteLine(sum);
+                        break;
+                    case '-':
+                        myCalc.CalcEvent += HandllerMinus;
+                        sum = myCalc.EventInvoker(num1, num2);
+                        Console.WriteLine(sum);
+                        break;
+                    case '*':
+                        myCalc.CalcEvent += HandllerMultyple;
+                        sum = myCalc.EventInvoker(num1, num2);
+                        Console.WriteLine(sum);
+                        break;
+                    case '/':
+                        myCalc.CalcEvent += HandllerDivide;
+                        sum = myCalc.EventInvoker(num1, num2);
+                        Console.WriteLine(sum);
+                        break;
+                    case '%':
+                        myCalc.CalcEvent += (n, m) => n * m / 100;
+                        sum = myCalc.EventInvoker(num1, num2);
+                        Console.WriteLine(sum);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong operation");
+                        break;
+                }
+
+                Console.WriteLine(new string('-', 22));
+                Console.WriteLine(sum);
+                num1 = sum;
+                operation = char.Parse(Console.ReadLine());
+                num2 = float.Parse(Console.ReadLine());
+                Console.WriteLine("=");
+                //Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ OOP_(2nd_Course)/ConsoleApp2test/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        public delegate double ResultOfNumbers(double x, double y);
+
+        class Calculator
+        {
+
+
+            public event ResultOfNumbers NumbersOf;
+            public void EventInvoke(double x, double y)
+            {
+                NumbersOf.Invoke(x, y);
+            }
+            public double YNumber(double y)
+            {
+                try
+                {
+                    Console.WriteLine("Enter Y number");
+                    y = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please Enter Number!");
+
+                }
+                return y;
+
+            }
+
+            public double XNumber(double x)
+            {
+                try
+                {
+                    Console.WriteLine("Enter X number");
+                    x = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please Enter Number!");
+
+                }
+                return x;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            double x = 0.0;
+            double y = 0.0;
+            x = calculator.XNumber(x);
+            y = calculator.YNumber(y);
+            Console.WriteLine("Please Chose An Operation wich one you wont to do: +,-,*,/ ");
+            string Oper = Console.ReadLine();
+            switch (Oper)
+            {
+
+                case "-":
+                    calculator.NumbersOf += Sum;
+                    calculator.EventInvoke(x, y);
+                    break;
+                case "*":
+                    calculator.NumbersOf += Mult;
+                    calculator.EventInvoke(x, y);
+                    break;
+                case "/":
+                    calculator.NumbersOf += Divis;
+                    calculator.EventInvoke(x, y);
+                    break;
+                case "+":
+                    calculator.NumbersOf += Add;
+                    calculator.EventInvoke(x, y);
+                    break;
+
+            }
+            Console.ReadKey();
+
+        }
+        public static double Add(double x, double y)
+        {
+            double res = x + y;
+            Console.WriteLine("Result of Operation: " + res);
+            return res;
+        }
+        public static double Sum(double x, double y)
+        {
+            double res = x - y;
+            Console.WriteLine("Result of Operation: " + res);
+            return res;
+
+        }
+        public static double Mult(double x, double y)
+        {
+            double res = x * y;
+            Console.WriteLine("Result of Operation: " + res);
+            return res;
+
+        }
+        public static double Divis(double x, double y)
+        {
+            if (y != 0)
+            {
+                double res = x / y;
+                Console.WriteLine("Result of Operation: " + res);
+                return res;
+            }
+            else
+                return 0;
+        }
+        public static double SmallistOddNumber(double x, double y)
+        {
+            int i;
+            for (i = 1; i < x; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                    return i;
+                    break;
+                }
+
+
+            }
+            return i;
+        }
+
+
+    }
+}
