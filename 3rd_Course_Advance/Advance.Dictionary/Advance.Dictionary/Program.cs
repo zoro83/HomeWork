8454a822d0fcb78ec574695198a730dd58affb74
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-12-18 02:12:54 +0400

    Dictionary version 0.4

--- 3rd_Course_Advance/Advance.Dictionary/Advance.Dictionary/Program.cs
+++ 3rd_Course_Advance/Advance.Dictionary/Advance.Dictionary/Program.cs
@@ -12,33 +12,33 @@ namespace Advance.Dictionary
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Dictionary<char, string> charTranlsation = new Dictionary<char, string>(30);
-            charTranlsation.Add('a', "ա");      charTranlsation.Add('A', "Ա"); 
-            charTranlsation.Add('b', "բ");      charTranlsation.Add('B', "Բ");
-            charTranlsation.Add('c', "ց");      charTranlsation.Add('C', "Ց");
-            charTranlsation.Add('d', "դ");      charTranlsation.Add('D', "Դ");
-            charTranlsation.Add('e', "ե");      charTranlsation.Add('E', "Ե");
-            charTranlsation.Add('f', "ֆ");      charTranlsation.Add('F', "Ֆ");
-            charTranlsation.Add('g', "գ");      charTranlsation.Add('G', "Գ");
-            charTranlsation.Add('h', "հ");      charTranlsation.Add('H', "Հ");
-            charTranlsation.Add('i', "ի");      charTranlsation.Add('I', "Ի");
-            charTranlsation.Add('j', "ջ");      charTranlsation.Add('J', "Ջ");
-            charTranlsation.Add('k', "կ");      charTranlsation.Add('K', "Կ");
-            charTranlsation.Add('l', "լ");      charTranlsation.Add('L', "Լ");
-            charTranlsation.Add('m', "մ");      charTranlsation.Add('M', "Մ");
-            charTranlsation.Add('n', "ն");      charTranlsation.Add('N', "Ն");
-            charTranlsation.Add('o', "ո");      charTranlsation.Add('O', "Ո");
-            charTranlsation.Add('p', "պ");      charTranlsation.Add('P', "Պ");
-            charTranlsation.Add('q', "ք");      charTranlsation.Add('Q', "Ք");
-            charTranlsation.Add('r', "ր");      charTranlsation.Add('R', "Ր");
-            charTranlsation.Add('s', "ս");      charTranlsation.Add('S', "Ս");
-            charTranlsation.Add('t', "տ");      charTranlsation.Add('T', "Տ");
-            charTranlsation.Add('u', "ու");     charTranlsation.Add('U', "Ու");
-            charTranlsation.Add('v', "վ");      charTranlsation.Add('V', "Վ");
-            charTranlsation.Add('w', "վ");      charTranlsation.Add('W', "Վ");
-            charTranlsation.Add('x', "խ");      charTranlsation.Add('X', "Խ");
-            charTranlsation.Add('y', "յ");      charTranlsation.Add('Y', "Յ");
-            charTranlsation.Add('z', "զ");      charTranlsation.Add('Z', "Զ");
-            charTranlsation.Add('@', "ը");      charTranlsation.Add('.', ":");
+            charTranlsation.Add('a', "ա"); charTranlsation.Add('A', "Ա");
+            charTranlsation.Add('b', "բ"); charTranlsation.Add('B', "Բ");
+            charTranlsation.Add('c', "ց"); charTranlsation.Add('C', "Ց");
+            charTranlsation.Add('d', "դ"); charTranlsation.Add('D', "Դ");
+            charTranlsation.Add('e', "ե"); charTranlsation.Add('E', "Ե");
+            charTranlsation.Add('f', "ֆ"); charTranlsation.Add('F', "Ֆ");
+            charTranlsation.Add('g', "գ"); charTranlsation.Add('G', "Գ");
+            charTranlsation.Add('h', "հ"); charTranlsation.Add('H', "Հ");
+            charTranlsation.Add('i', "ի"); charTranlsation.Add('I', "Ի");
+            charTranlsation.Add('j', "ջ"); charTranlsation.Add('J', "Ջ");
+            charTranlsation.Add('k', "կ"); charTranlsation.Add('K', "Կ");
+            charTranlsation.Add('l', "լ"); charTranlsation.Add('L', "Լ");
+            charTranlsation.Add('m', "մ"); charTranlsation.Add('M', "Մ");
+            charTranlsation.Add('n', "ն"); charTranlsation.Add('N', "Ն");
+            charTranlsation.Add('o', "ո"); charTranlsation.Add('O', "Ո");
+            charTranlsation.Add('p', "պ"); charTranlsation.Add('P', "Պ");
+            charTranlsation.Add('q', "ք"); charTranlsation.Add('Q', "Ք");
+            charTranlsation.Add('r', "ր"); charTranlsation.Add('R', "Ր");
+            charTranlsation.Add('s', "ս"); charTranlsation.Add('S', "Ս");
+            charTranlsation.Add('t', "տ"); charTranlsation.Add('T', "Տ");
+            charTranlsation.Add('u', "ու"); charTranlsation.Add('U', "Ու");
+            charTranlsation.Add('v', "վ"); charTranlsation.Add('V', "Վ");
+            charTranlsation.Add('w', "վ"); charTranlsation.Add('W', "Վ");
+            charTranlsation.Add('x', "խ"); charTranlsation.Add('X', "Խ");
+            charTranlsation.Add('y', "յ"); charTranlsation.Add('Y', "Յ");
+            charTranlsation.Add('z', "զ"); charTranlsation.Add('Z', "Զ");
+            charTranlsation.Add('@', "ը"); charTranlsation.Add('.', ":");
 
             Console.WriteLine("Latin leters armenian.ConvertTo(armenian)");
             string inputText = "";
@@ -57,7 +57,7 @@ namespace Advance.Dictionary
 
             for (int i = 0; i < inputText.Length; i++)
             {
-                if (inputText[i] >= '@' && inputText[i] <= 'Z' || inputText[i] >= 'a' && inputText[i] <= 'z')
+                if (inputText[i] >= '@' && inputText[i] <= 'Z' || inputText[i] >= 'a' && inputText[i] <= 'z' || inputText[i] == '.')
                 {
                     //InputByLetters[i] = inputText[i];
                     outputText += charTranlsation[inputText[i]];
@@ -68,13 +68,9 @@ namespace Advance.Dictionary
                 }
             }
             Console.WriteLine(outputText);
-            //Console.WriteLine(Convert.ToInt32('a')); // 97
-            //Console.WriteLine(Convert.ToInt32('A')); // 65
-            //Console.WriteLine(Convert.ToInt32('z')); // 122
-            //Console.WriteLine(Convert.ToInt32('Z')); // 90
-            //Console.WriteLine(Convert.ToInt32('@')); // 64
+
             Console.ReadKey();
-           
+
         }
     }
 }
