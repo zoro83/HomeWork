04decea9a7144744479c89703b02546041288a98
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2019-01-23 00:42:39 +0400

    Commit All

---
+++ ThirdStage_Sorting Algorithms/Algorithms.MergeSort/Algorithms.MergeSort/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection.Sort
+{
+    // https://www.tutorialspoint.com/data_structures_algorithms/selection_sort_algorithm.htm
+    // https://www.cs.usfca.edu/~galles/visualization/ComparisonSort.html
+    class Program
+    {
+        public static List<int> MergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+                return unsorted;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            int middle = unsorted.Count / 2;
+            for (int i = 0; i < middle; i++)  //Dividing the unsorted list
+            {
+                left.Add(unsorted[i]);
+            }
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right);
+        }
+
+        private static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())  //Comparing First two elements to see which is smaller
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());      //Rest of the list minus the first element
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int>
+                {3,7,4,4,6,5,8,0,0,-4,88,2,1,-3,7};
+            arr = MergeSort(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
